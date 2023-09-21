bd0afe767fb90363dc9ee7533787216a85ebc9fd
jacze.dev <jacze.dev@gmail.com>
2023-09-21 11:53:18 +0300

    CUSTOMIZING USER MODEL, WORKING ON AUTHENTICATIONS

---
+++ storefront2/store/serializers.py
@@ -0,0 +1,101 @@
+from decimal import Decimal
+from itertools import count
+from rest_framework import serializers
+
+from store.models import CartItem, Product,Collection, Review, Cart
+
+class CollectionSerializer(serializers.ModelSerializer):
+   class Meta:
+        model = Collection
+        fields = ['id','title','products_count']
+   products_count = serializers.IntegerField(read_only = True)
+
+
+        
+
+class ProductSerializers(serializers.ModelSerializer):
+   class Meta:
+       model = Product
+       fields = ['id','title','slug','description','inventory','unit_price','price_with_tax','collection']
+   
+
+#    id = serializers.IntegerField()
+#    title = serializers.CharField(max_length = 255)
+#    price = serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')
+   price_with_tax = serializers.SerializerMethodField(method_name='claculate_tax')
+#    collection = CollectionSerializer()
+   # collection = serializers.StringRelatedField()
+#    collection = serializers.PrimaryKeyRelatedField(
+#        queryset = Collection.objects.all()
+#    )
+
+   def claculate_tax(self,product:Product):
+       return product.unit_price + product.unit_price * Decimal(0.15)
+   
+class ReviewSerializer(serializers.ModelSerializer):
+      class Meta:
+         model = Review
+         fields = ['id','name','description']
+
+      def create(self, validated_data):
+          product_id = self.context['product_id']
+          return Review.objects.create(Product_id = product_id,**validated_data)
+      
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','title','unit_price']
+
+class CartItemSerializaer(serializers.ModelSerializer):
+    product = SimpleProductSerializer(read_only = True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+    def total(self,cartitem:CartItem):
+        return cartitem.quantity * cartitem.product.unit_price
+        
+    class Meta:
+        model = CartItem
+        fields = ['id','product','quantity','total_price']
+      
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only = True)
+    items = CartItemSerializaer(many = True,read_only = True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+    
+    def total(self,cart:Cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+
+    class Meta:
+        model = Cart
+        fields = ['id','items','total_price']
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self,value):
+       if not Product.objects.filter(pk = value).exists():
+           raise serializers.ValidationError('No product with Id this')
+       return value
+        
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id,product_id = product_id)
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+           self.instance = CartItem.objects.create(cart_id = cart_id,**self.validated_data)
+
+        return self.instance
+
+    class Meta:
+        model = CartItem
+        fields = ['id','product_id','quantity']
+        
+class UpdateCarItemSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = CartItem
+      fields = ['quantity']
